ea44c33d006aff939a52e981e5f2d81d973ae283
yrys_nm <yrys.n3m@gmail.com>
2022-07-07 16:06:30 +0600

    changed palette

--- src/pages/Catalog/app/CatalogsApp.js
+++ src/pages/Catalog/app/CatalogsApp.js
@@ -1,81 +1,49 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { usePalette } from 'react-palette'
-import AppInputImg from "../app-inputImg/AppInputImg";
+import { Component } from "react";
+import { ColorExtractor } from "react-color-extractor";
 
-const CatalogsApp = () => {
+class CatalogsApp extends Component {
+    state = {
+        colors: [],
+    } 
 
-    const [product, setProduct] = useState([]);
-    const [imgSrc, setImgSrc] = useState("");
-    const [dataImgUrl, setDataImgUrl] = useState("");
-    const [colorImgInput, setColorImgInput] = useState("");
+    renderSwatches = () => {
+        const {colors} = this.state;
 
+        return colors.map((color, id) => {
+            return(
+                <div key={id} style={{backgroundColor: color, width: 100, height: 100}} />
 
-    useEffect(() => {
-        axios.get("http://localhost:4000/products")
-            .then(res => {
-                setProduct(res.data);
-                res.data.map(item => {
-                    return setDataImgUrl(item.image)
-                })
-            }).catch((e) => console.log(e));
-    }, []);
-
-
-    /**
-     * 
-     * @param {TODO create compare color in images and change state !dataImgURL!} imgSrc 
-     * 
-     * 
-     */
-
-
-
-    const getImgSrc = (imgSrc) => {
-        setImgSrc(imgSrc);
+            );
+        });
     }
 
-    const getColorImgInput = (color) => {
-        setColorImgInput(color);
+    getColors = colors => {
+        this.setState(state => ({
+            colors: [...state.colors, ...colors]
+        }))
+
     }
+    render() {
+        return(
+            <div>
+            <ColorExtractor getColors={this.getColors}>
+              <img
+                src="https://i.pinimg.com/736x/cc/3b/77/cc3b770949eb3c8216a3e8d4c43e71c5.jpg"
+                style={{ objectFit: "cover" }}
+              alt="test img"/>
+            </ColorExtractor>
+            <div
+              style={{
+                marginTop: 20,
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              {this.renderSwatches()}
+            </div>
+          </div>
+        );
+    }
+}   
 
-    const { data} = usePalette(dataImgUrl);
-
-    return (
-        <section className="catalogs">
-            <div className="container">
-                <h1>Test Product</h1>
-                <button>
-                    <a href="/">back main</a>
-                </button>
-
-                <AppInputImg getImgSrc={getImgSrc} getColorImgInput={getColorImgInput} />
-
-                <div className="blocktest">
-                    {
-                        product.map(item => {
-                            console.log(data.vibrant === colorImgInput, data, colorImgInput);
-                            const res = (colorImgInput) ?  <img src={imgSrc} alt="input img" /> : (data.vibrant === colorImgInput) ?   <img src={item.image} alt={item.type} /> : null;
-                            return (
-                                <div key={item.id} className="imgList">
-                                    {res}
-                                </div>
-                            );
-
-                        })
-                    }
-                </div>
-
-                <div style={{ "color": data.vibrant, "fontSize": 50 }}>
-                    Text with the data color
-                </div>
-
-                <div style={{ "color": colorImgInput, "fontSize": 50 }}>
-                    Test text for color input
-                </div>
-            </div >
-        </section >
-    );
-}
-
-export default CatalogsApp;
+export default CatalogsApp 
